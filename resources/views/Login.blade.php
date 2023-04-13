99b6e1bb0cea42162781e3c6e11faa6f05e270b7
Jorge <jorgechavarria667@gmail.com>
2023-04-13 14:25:45 -0600

    LoginView

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login Form Design One | Fazt</title>
+    <link rel="stylesheet" href="/css/login.css">
+  </head>
+  <body>
+
+  <div class="header">
+  <h1>¡Bienvenido!</h1>
+</div>
+
+    <div class="login-box">
+      <img src="img/logo.png" class="avatar" alt="Avatar Image">
+      <br>
+      <h1>Identificate</h1>
+      <br>
+      <form>
+        <!-- USERNAME INPUT -->
+        <label for="username">Uusario</label>
+        <input type="text" placeholder="Digite su usario">
+        <br>
+        <!-- PASSWORD INPUT -->
+        <br>
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Digite su contraseña">
+        <br><br>
+        <input type="submit" value="Log In">
+        <a href="#">Lost your Password?</a><br>
+        <a href="#">Don't have An account?</a>
+      </form>
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Vista1');
+    return view('Login');
 });
 
 
