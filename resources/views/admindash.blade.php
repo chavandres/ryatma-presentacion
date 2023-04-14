0a2da3b6ef3dba1cec922b4b03e2f68380863bf9
Andres Chavarria <adrchb24@gmail.com>
2023-04-13 18:10:15 -0600

    Admin view

---
+++ resources/views/admindash.blade.php
@@ -0,0 +1,208 @@
+@extends('layouts.admin')
+
+@section('content')
+      <div class="mask d-flex align-items-center h-100">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-12">
+              <div class="card shadow-2-strong" style="background-color: #f5f7fa;">
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table table-borderless mb-0">
+                      <thead>
+                        <tr>
+                          <th scope="col">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                            </div>
+                          </th>
+                          <th scope="col">Nombre</th>
+                          <th scope="col">Correo</th>
+                          <th scope="col">Telefono</th>
+                          <th scope="col">Rol</th>
+                          <th scope="col">Acciones</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                            </div>
+                          </th>
+                          <td>Rodrigo Alvarez</td>
+                          <td>ralvarez@ryatma.com</td>
+                          <td>2290-2290</td>
+                          <td>Agente de Ventas</td>
+                          <td>
+                            <button type="button" class="btn btn-info btn-sm px-3">
+                                Editar
+                            </button>
+                            <button type="button" class="btn btn-danger btn-sm px-3">
+                                Eliminar
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Pedro Chaves</td>
+                            <td>pchaves@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Agente de Ventas</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Andres Chavarria</td>
+                            <td>achava@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Administrador</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Cesar Melendez</td>
+                            <td>cmelendez@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Agente de Ventas</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Alejandro Chavarria</td>
+                            <td>achavarria@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Administrador</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Helen Corrales</td>
+                            <td>hcorrales@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Agente de Ventas</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Mario Alberto</td>
+                            <td>malberto@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Administrador</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Jose Brenes</td>
+                            <td>jbrenes@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Administrador</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">
+                              <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked/>
+                              </div>
+                            </th>
+                            <td>Felipe Villalobos</td>
+                            <td>fvillalobos@ryatma.com</td>
+                            <td>2290-2290</td>
+                            <td>Administrador</td>
+                            <td>
+                              <button type="button" class="btn btn-info btn-sm px-3">
+                                  Editar
+                              </button>
+                              <button type="button" class="btn btn-danger btn-sm px-3">
+                                  Eliminar
+                              </button>
+                            </td>
+                          </tr>
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,90 @@
+{{-- BEGIN SIDEBAR --}}
+<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+    <div class="sidebar-brand d-none d-md-flex">
+        <img class="sidebar-brand-full" width="118" height="46" src="img/logo.png"
+            alt="Ryatma S.A">
+        <img class="sidebar-brand-narrow" width="46" height="46" src="img/logo.png"
+            alt="Ryatma S.A">
+    </div>
+    <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+        <li class="nav-title">Administracion del Sistema</li>
+        <li class="nav-item"><a class="nav-link" href="index.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                </svg> Usuarios</a></li>
+        <li class="nav-title">Operaciones de Negocio</li>
+        <li class="nav-item"><a class="nav-link" href="colors.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-car-alt"></use>
+                </svg> Productos</a></li>
+        <li class="nav-item"><a class="nav-link" href="typography.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-graph"></use>
+                </svg> Proveeduria</a></li>
+    </ul>
+    <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
+</div>
+{{-- END SIDEBAR --}}
+<div style="padding-left:var(--cui-sidebar-occupy-start,0);" class="wrapper d-flex flex-column min-vh-100 bg-light">
+    <header class="header header-sticky mb-4">
+        <div class="container-fluid">
+            <a class="header-brand d-md-none" href="#">
+                <svg width="118" height="46" alt="CoreUI Logo">
+                    <use xlink:href="assets/brand/coreui.svg#full"></use>
+                </svg></a>
+            <ul class="header-nav ms-auto">
+                
+            </ul>
+            <ul class="header-nav ms-3">
+                <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#"
+                        role="button" aria-haspopup="true" aria-expanded="false">
+                        <div class="avatar avatar-md"><img class="avatar-img" src="img/avatars/8.jpg"
+                                alt="user@email.com"></div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end pt-0">
+                        <div class="dropdown-header bg-light py-2">
+                            <div class="fw-semibold">Account</div>
+                        </div><a class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
+                            </svg> Updates<span class="badge badge-sm bg-info ms-2">42</span></a><a
+                            class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
+                            </svg> Messages<span class="badge badge-sm bg-success ms-2">42</span></a><a
+                            class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-task"></use>
+                            </svg> Tasks<span class="badge badge-sm bg-danger ms-2">42</span></a><a
+                            class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-comment-square"></use>
+                            </svg> Comments<span class="badge badge-sm bg-warning ms-2">42</span></a>
+                        <div class="dropdown-header bg-light py-2">
+                            <div class="fw-semibold">Settings</div>
+                        </div><a class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                            </svg> Profile</a><a class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
+                            </svg> Settings</a><a class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
+                            </svg> Payments<span class="badge badge-sm bg-secondary ms-2">42</span></a><a
+                            class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
+                            </svg> Projects<span class="badge badge-sm bg-primary ms-2">42</span></a>
+                        <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                            </svg> Lock Account</a><a class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                            </svg> Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </header>
